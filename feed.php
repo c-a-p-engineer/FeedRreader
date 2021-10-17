cf83c68f46be410cc1b45f2ae8c07713ab514e50
こぴぺたん <takahiro.inoue.0508+github@gmail.com>
2021-10-18 08:39:13 +0900

    Bootstrap導入

--- feed.php
+++ feed.php
@@ -25,9 +25,35 @@ foreach ($setting['urls'] as $url) {
 // ソートを変更
 $posts = sortByKey('date', SORT_DESC, $posts);
 
-$html = '<ul>';
+$html = <<<HTML
+<!doctype html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Rss Reader</title>
+  </head>
+  <body>
+HTML;
+
+$html .= '<ul class="list-group">';
 foreach($posts as $post){
-    $html .= '<li>' . $post['date'] . ' | ' . $post['title'] . ' | ' . $post['siteTitle'] . '</li>';
+    $html .= '<a href="'.$post['link'].'" class="list-group-item list-group-item-action" target="_blank">' . $post['date'] . ' | ' . $post['title'] . ' | ' . $post['siteTitle'] . '</a>';
 }
-$html .= '</ul>';
+$html .= '</ul">';
+
+$html .= <<<HTML
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+HTML;
 file_put_contents('./docs/index.html', $html);
